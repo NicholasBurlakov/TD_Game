f13137b44202204c5bd3c74def3ccdf5bd586a3a
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-12-14 19:27:52 -0800

    fixed love2d engine

--- src/main.lua
+++ src/main.lua
@@ -2,10 +2,17 @@
 
 function love.load()
     Object = require "classic"
+    lightGrass = love.graphics.newImage("/sprites/Environment/Grass1.png")
+    darkGrass = love.graphics.newImage("/sprites/Environment/Grass2.png")
+    dirt = love.graphics.newImage("/sprites/Environment/Dirt.png")
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.draw(lightGrass, 100, 100)
+    love.graphics.draw(darkGrass, 150, 100)
+    love.graphics.draw(dirt, 200, 100)
+
 end
